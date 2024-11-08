44d2bc8a999909766d795fb84774395d01f416a5
gerard415 <chinedumike85@gmail.com>
2024-11-08 10:31:23 -0500

    project 12

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const mainRouter = require("./routes/index");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -10,6 +11,18 @@ mongoose
     console.log("Connected to DB");
   })
   .catch(console.error);
+
+app.use(express.json());
+
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '672cffdaad402e4b85a60560'
+  };
+  next();
+});
+
+app.use("/", mainRouter);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
