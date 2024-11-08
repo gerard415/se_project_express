173255a6df7fe6463181ca7ecc2e1295d3d37ffc
gerard415 <chinedumike85@gmail.com>
2024-11-08 10:30:44 -0500

    users controllers

---
+++ controllers/users.js
@@ -0,0 +1,52 @@
+const User = require("../models/user");
+const {
+  NOT_FOUND,
+  SERVER_ERROR,
+  BAD_REQUEST,
+} = require("../utils/errors");
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.status(200).send(users))
+    .catch((err) => {
+      console.error(err);
+      return res.status(SERVER_ERROR).send({ message: "An error has occurred on the server." });
+    });
+};
+
+const createUser = (req, res) => {
+  const { name, avatar } = req.body;
+
+  User.create({ name, avatar })
+    .then((user) => res.status(201).send(user))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        return res.status(BAD_REQUEST).send({ message: "Invalid data" });
+      }
+      return res.status(SERVER_ERROR).send({ message: "An error has occurred on the server." });
+    });
+};
+
+const getUser = (req, res) => {
+  const { userId } = req.params;
+  User.findById(userId)
+    .orFail()
+    .then((user) => res.send(user))
+    .catch((err) => {
+      if (err.name === "DocumentNotFoundError") {
+        return res
+          .status(NOT_FOUND)
+          .send({ message: "Requested resource not found" });
+      }
+      if (err.name === "CastError") {
+        return res
+          .status(BAD_REQUEST)
+          .send({ message: "Invalid data" });
+      }
+      return res
+        .status(SERVER_ERROR)
+        .send({ message: "An error has occurred on the server" });
+    });
+};
+
+module.exports = { getUsers, createUser, getUser };
